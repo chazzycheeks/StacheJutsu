ed3f335203c9c8344793777ff877a88efb15f2f1
Cheeks <chalty2bass@gmail.com>
2025-05-25 11:49:35 +0100

    key shortcuts

--- Assets/Scripts/GameOverScreen.cs
+++ Assets/Scripts/GameOverScreen.cs
@@ -4,7 +4,19 @@ using UnityEngine.SceneManagement;
 
 public class GameOverScreen : MonoBehaviour
 {
-    
+    private void Update()
+    {
+        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SceneManager.LoadScene(1);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            SceneManager.LoadScene(0);
+        }
+    }
     public void RestartButton()
     {
         SceneManager.LoadScene(1);
